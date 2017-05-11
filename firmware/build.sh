dd1174e267a2cb8943af8c6295bf5d854088d007
Ismael Luceno <ismael@iodev.co.uk>
2017-05-11 17:52:10 -0300

    Replace build.sh with Makefile

--- firmware/build.sh
+++ firmware/build.sh
@@ -1,7 +1,3 @@
-sdcc --code-loc 0x0000 --xram-loc 0x0000 crtinfo.c
-objcopy crtinfo.ihx ../crtinfo.u4 -I ihex -O binary
-dd if=/dev/zero of=/tmp/zeroes-64k.rom bs=1 count=65536
-cat ../crtinfo.u4 /tmp/zeroes-64k.rom > /tmp/tmp.rom
-split /tmp/tmp.rom -b 65536
-ls -lh xaa
-mv xaa ~/ROM_DUMPS/FSanches/qmusica/2400.ci4
+#!/bin/sh
+make &&
+cp crtinfo.rom ~/ROM_DUMPS/FSanches/qmusica/2400.ci4
